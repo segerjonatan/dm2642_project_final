a49035272e27d032d1a8abf22d65ea32890e0553
Jonatan Seger <jseger@kth.se>
2018-05-21 11:52:33 +0200

    first commit

---
+++ src/components/Confirm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Confirm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      confirmOrder: false 
+    }
+  render() {
+    let page = null;
+    switch(this.state.confirmOrder) {
+      default:
+        let
+    }
+  }
+}
+}
+
+
+
+/*const Confirm = (props) => {
+  return (
+    <div> Hejhej </div>
+  )
+}*/
